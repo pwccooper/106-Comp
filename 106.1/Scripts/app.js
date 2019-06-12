9e0a4e25da04e87911b2d6e1a3a25b7e22b66ef0
Paul W Cooper <48501976+pwccooper@users.noreply.github.com>
2019-06-11 22:43:14 -0400

    Add files via upload

--- 106.1/Scripts/app.js
+++ 106.1/Scripts/app.js
@@ -1,22 +1,21 @@
-
 var name = "Sergio";
 var todoList = [];
 var serverURL = 'http://restclass.azurewebsites.net';
 
-function init(){
-    
+function init() {
+
     var lbl = document.getElementById("lblTodo");
     lbl.innerText = "Get it done!";
 
-    
+
 
     var btn = document.getElementById("btnSave");
-  
 
-   btn.onclick = saveTodo;
+
+    btn.onclick = saveTodo;
 };
 
-function init2(){
+function init2() {
 
     var lbl = $("#lblTodo");
     lbl.innerText = "jQuery rules??";
@@ -28,32 +27,32 @@ function init2(){
 };
 
 
-function saveTodo(){
+function saveTodo() {
     var txt = $("#txtTodo");
     var todoText = txt.val();
     console.log(todoText);
 
-    if(todoText.length < 1) {
+    if (todoText.length < 1) {
         txt.addClass('error');
 
         alert("You entry was blank please try again!");
         console.log("Nothing was stored.");
 
-    } else{
-    
-    txt.removeClass('error');
-    todoList.push(todoText);
-    txt.val('');
-    
+    } else {
 
-    sendToServer(todoText);
+        txt.removeClass('error');
+        todoList.push(todoText);
+        txt.val('');
+
+
+        sendToServer(todoText);
 
     };
-    
+
 };
 
-function sendToServer(text){
-    
+function sendToServer(text) {
+
     var todoItem = {
         text: text,
         user: "Paul",
@@ -66,71 +65,72 @@ function sendToServer(text){
         contentType: "application/json",
         data: JSON.stringify(todoItem),
 
-        success: function(res){
+        success: function (res) {
             console.log("server says: ", res);
             todoList.push(res);
             displayTodo(res.text, res.id);
         },
-        error: function(error){
+        error: function (error) {
             console.error("*Error:", error);
         }
     });
-    
+
 }
 
 
 
-function getDataFromServer(){
+function getDataFromServer() {
 
-$.ajax({
-    url: serverURL + "/API/points",
-    type: "GET",
-    success: function(res){
-      console.log("Server Response", res);  
+    $.ajax({
+        url: serverURL + "/API/points",
+        type: "GET",
+        success: function (res) {
+            console.log("Server Response", res);
 
-      for (var i=0; i < res.length; i++){
-        var item = res[i];
+            for (var i = 0; i < res.length; i++) {
+                var item = res[i];
 
-        if (item.user == "Paul"){
-            todoList.push(item);
-            displayTodo(item.text, item.id);
+                if (item.user == "Paul") {
+                    todoList.push(item);
+                    console.log(item);
+                    displayTodo(item.text, item.id);
+                }
             }
-        }
-    },
+        },
 
-    error: function(error){
-        console.log("Error!",error);
-    }
-});
+        error: function (error) {
+            console.log("Error!", error);
+        }
+    });
 }
 
 function displayTodo(simpleText, id) {
 
-var ul = $("#todoList");
+    var ul = $("#todoList");
 
-var li = "<li id='"+ id +"' class='list-group-item'>"  + simpleText + "<button class='btn-danger btn-sm btn-info pull-right btn-done' onclick=markDone("+ id +");> Done! </button></li>"
+    var li = "<li id='" + id + "' class='list-group-item'>" + simpleText + "<button class='btn-danger btn-sm btn-info pull-right btn-done' onclick=markDone(" + id + ");> Done! </button></li>"
 
-ul.append(li);
+    ul.append(li);
 
 };
 
 function displayDone(simpleText, id) {
 
     var ul = $("#doneList");
-    
-    var li = "<li class='list-group-item done-item'>"  + simpleText +  "</li>"
-    
+
+    var li = "<li class='list-group-item done-item'>" + simpleText + "</li>"
+
     ul.append(li);
-    
-    };
+
+};
 
 
 
-function markDone(id){
+function markDone(id) {
     console.log("done:", id);
     var theItem;
-    for(var i=0; i < todoList.length; i++){
-        if(todoList[i].id == id){
+    for (var i = 0; i < todoList.length; i++) {
+        if (todoList[i].id == id) {
             theItem = todoList[i];
             break;
         }
@@ -140,16 +140,18 @@ function markDone(id){
 
     displayDone(theItem.text, theItem.id);
 
+    $("#" + id).remove();
+
     $.ajax({
-        url:serverURL + "/API/points",
-        type:"PUT",
+        url: serverURL + "/API/points",
+        type: "PUT",
         data: JSON.stringify(theItem),
         contentType: "application/json",
-        success: function(res){
+        success: function (res) {
             console.log("Server says: ", res);
 
         },
-        error: function(error){
+        error: function (error) {
             console.error("*Error", error);
         }
     });
